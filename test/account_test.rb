db173c9a83a584000399c4df4ec4034ea43004e3
Jason Weathered <jason@jasoncodes.com>
2011-03-13 16:46:56 +1000

    Add test for direct `validates_all` usage.

--- test/account_test.rb
+++ test/account_test.rb
@@ -3,6 +3,9 @@ require 'test_helper'
 class Account < ActiveRecord::Base
   validates_presence_of_either :email_address, :login_name, :if => Proc.new {|user| user.full_name != 'dan'}
   validates_different :primary_account_holder_id, :secondary_account_holder_id, :suppress_on_blanks => true
+  validates_all :email_address, :secondary_account_holder_id do |record, attrs, values|
+    record.errors.add :base, "cannot have both email address and secondary account holder" if values.compact.size > 1
+  end
 end
 
 class AccountTest < ActiveSupport::TestCase
@@ -52,5 +55,12 @@ class AccountTest < ActiveSupport::TestCase
   def test_should_not_have_error_on_base_if_account_holder_ids_are_both_blank
     @account.valid?
     assert_no_match(/Primary account holder cannot be the same as Secondary account holder/i, @account.errors[:base].join)
+  end
+  
+  def test_should_have_error_with_both_email_address_and_secondary_account_holder_id
+    @account.email_address = 'foo@example.com'
+    @account.secondary_account_holder_id = 42
+    @account.valid?
+    assert_match(/cannot have both email address and secondary account holder/i, @account.errors[:base].join)
   end
 end
